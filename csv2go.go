d6446e17ef69b46a296685310849244a9c7a7335
Yuanqing Cai <ckjacket@gmail.com>
2015-03-17 00:15:18 +0800

    add methods for Decoder of csv2go

--- csv2go.go
+++ csv2go.go
@@ -4,19 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
-	"os"
-	"reflect"
-	"time"
 )
-
-type Account struct {
-	CardId              string
-	Name, Mobile        string
-	CreateDate          time.Time
-	SaveAmt, ConsumeAmt float64
-	LeftAmt             float64
-	Items               []string
-}
 
 type Decoder struct {
 	reader     *csv.Reader
@@ -27,17 +15,55 @@ type Decoder struct {
 	bHandler   funcBoolean
 }
 
+type csvHeader map[int]string
 type funcBoolean func(string) bool
 
+// Comma set record delimiter, default is ','
+func (d *Decoder) Comma(s rune) *Decoder {
+	d.reader.Comma = s
+	return d
+}
+
+// Comment set comment character for start of line
+func (d *Decoder) Comment(s rune) *Decoder {
+	d.reader.Comment = s
+	return d
+}
+
+// FieldsPerRecord set number of expect fields per record
+func (d *Decoder) FieldsPerRecord(n int) *Decoder {
+	d.reader.FieldsPerRecord = n
+	return d
+}
+
+// LazyQuotes set allow lazy quotes
+func (d *Decoder) LazyQuotes(b bool) *Decoder {
+	d.reader.LazyQuotes = b
+	return d
+}
+
+// TrailingComma ignored
+func (d *Decoder) TrailingComma(b bool) *Decoder {
+	d.reader.TrailingComma = b
+	return d
+}
+
+// TrimLeadingSpace set trim leading space
+func (d *Decoder) TrimLeadingSpace(b bool) *Decoder {
+	d.reader.TrimLeadingSpace = b
+	return d
+}
+
 // setBooleanHandler custom boolean handler for fields
-func (d *Decoder) setBooleanHandler(f funcBoolean) {
+func (d *Decoder) setBooleanHandler(f funcBoolean) *Decoder {
 	d.bHandler = f
+	return d
 }
 
 // DoBoolean return boolean of this field
 func (d *Decoder) DoBoolean(field string) bool {
 	if d.bHandler != nil {
-		return d.bHandler()
+		return d.bHandler(field)
 	}
 	if len(field) > 0 {
 		return true
@@ -58,44 +84,25 @@ func (d *Decoder) Close() error {
 	return nil
 }
 
-func (d *Decoder) decode(i interface{}) {
+func (d *Decoder) Decode(i interface{}) error {
+	var err error
 	for {
 		record, err := d.Read()
 		if err == io.EOF {
 			fmt.Println("Read out of file")
 			break
 		} else if err != nil {
-			return
+			return err
 		}
 		for i, field := range record {
 			fmt.Println(i, field)
 		}
 	}
+	return err
 }
 
-func Test() {
-	Test2(Account{})
-}
-
-func Test2(i interface{}) {
-	fmt.Println("Test")
-	typ := reflect.TypeOf(i)
-	fmt.Println("type of account:", typ, "contains", typ.NumField(), "fields", "with kind", typ.Kind())
-	for i := 0; i < typ.NumField(); i++ {
-		st := typ.Field(i)
-		fmt.Println("the", i, "field is", st)
-	}
-
-}
-
-func NewDecoder(file string) *Decoder {
-	f, err := os.Open(file)
-	if err != nil {
-		fmt.Println("Open", file, "failed")
-		return nil
-	}
-	decoder := new(Decoder)
-	decoder.fd = f
-	decoder.Reader = csv.NewReader(f)
+func NewDecoder(in io.ReadCloser) *Decoder {
+	decoder := &Decoder{reader: csv.NewReader(in)}
+	decoder.csvReader = in
 	return decoder
 }
